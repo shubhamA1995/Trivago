0a62c486f0e001d25f1be3e1ed0886266276fc77
unknown <shubhamanantwar9418@gmail.com>
2022-01-16 08:28:43 +0530

    push new project

---
+++ src/test/java/com/example/trivago/TrivagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trivago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrivagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
